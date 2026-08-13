2cbd5ab3b1871c4a189c73e2814f6febfa845aaf
MohammedAshraf2006 <did9m9did@gmail.com>
2026-08-13 15:43:54 +0300

    v2.2

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior() {
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition
+    if (to.hash) {
+      return {
+        el: to.hash,
+        top: 90, // مساحة تعويض ارتفاع الـ Header الثابت (fixed)
+        behavior: 'smooth'
+      }
+    }
     return { top: 0 }
   }
 })
